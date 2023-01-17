09ef2c39f64dc2dabfcfe2b636a97e1ff2e9c6f4
rahulrathore2110 <rahulg27727@gmail.com>
2023-01-18 00:16:50 +0530

    Added : Project Setup

---
+++ src/main/java/com/onlinebusadda/OnlineBusAddaApplication.java
@@ -0,0 +1,13 @@
+package com.onlinebusadda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBusAddaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBusAddaApplication.class, args);
+	}
+
+}
