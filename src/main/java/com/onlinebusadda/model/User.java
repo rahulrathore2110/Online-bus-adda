adf0f7399723101068aa4076f7fada03a703feac
shashi1049 <shashi290396@gmail.com>
2023-01-19 19:52:29 +0530

    user and admin

---
+++ src/main/java/com/onlinebusadda/model/User.java
@@ -0,0 +1,59 @@
+package com.onlinebusadda.model;
+
+import java.util.Objects;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.UniqueElements;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer userLoginId;
+	
+	private String userName;
+	
+	@Size(min = 6, message="Password length must be between 6 to 10 character")
+	@Size(max = 10, message = "Password length must be between 6 to 10 character")
+	private String password;
+
+	private String firstName;
+	
+	private String lastName;
+	
+	private Long contact;
+	
+	@Email(message = "Please enter valid email Id")
+	private String email;
+	
+	//@JsonIgnore
+	//@OneToOne
+	//private Reservation reservation;
+
+}
