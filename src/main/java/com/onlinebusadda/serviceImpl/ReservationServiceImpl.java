0eee8f7c58d5e29d8b50308860f9a77fe25a6da6
IInnffiinniittyy <tanuj813@gmail.com>
2023-01-20 22:25:48 +0530

    submit

--- src/main/java/com/onlinebusadda/serviceImpl/ReservationServiceImpl.java
+++ src/main/java/com/onlinebusadda/serviceImpl/ReservationServiceImpl.java
@@ -1,12 +1,19 @@
 package com.onlinebusadda.serviceImpl;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.onlinebusadda.exception.AdminException;
+import com.onlinebusadda.exception.BusException;
 import com.onlinebusadda.exception.ReservationException;
+import com.onlinebusadda.exception.UserException;
+import com.onlinebusadda.model.Bus;
+import com.onlinebusadda.model.CurrentUserSession;
 import com.onlinebusadda.model.Reservation;
-import com.onlinebusadda.repository.AdminSessionRepo;
+import com.onlinebusadda.model.User;
+import com.onlinebusadda.repository.BusRepo;
 import com.onlinebusadda.repository.ReservationRepo;
 import com.onlinebusadda.repository.SessionRepo;
 import com.onlinebusadda.repository.UserRepo;
@@ -15,43 +22,22 @@ import com.onlinebusadda.service.ReservationService;
 @Service
 public class ReservationServiceImpl implements ReservationService {
 
-	
-	@Autowired
-	private ReservationRepo reservationRepo;
-	
-	@Autowired
-	private SessionRepo sessionRepo;
-	
-	@Autowired
-	private AdminSessionRepo adminSessionRepo;
-	
-	
-	@Autowired
-	private UserRepo userRepo;
-	
-	
-	
-	
 	@Override
-	public Reservation addReservation(Reservation reservation, String key) throws ReservationException {
+	public Reservation addReservation(Reservation reservation, Integer busId, Integer userLoginId)
+			throws ReservationException, BusException, UserException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Reservation updateReservation(Reservation reservation, String key) throws ReservationException {
+	public Reservation deleteReservation(Integer reservationId, String key)
+			throws ReservationException, BusException, UserException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Reservation deleteReservation(Integer reservationId, String key) throws ReservationException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Reservation viewReservation(Integer reservationId, String key) throws ReservationException {
+	public Reservation viewReservation(Integer reservationId, String key) throws ReservationException, AdminException {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -68,4 +54,5 @@ public class ReservationServiceImpl implements ReservationService {
 		return null;
 	}
 
+	
 }
